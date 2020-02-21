b778e5b2304fa81a0b52627c0f3c2c0ad73295fe
giada1198 <giada.sun@rocketmail.com>
2020-02-21 11:00:44 -0800

    finished version

--- ne-nuzhno-tak-staratsya/ne-nuzhno-tak-staratsya.ino
+++ ne-nuzhno-tak-staratsya/ne-nuzhno-tak-staratsya.ino
@@ -7,86 +7,77 @@
 
 CRGB leds[NUM_LEDS];
 
-float duration, cm, inches;
+bool hasBlackedOut = false;
+int color_state = 0;
+int pattern_state = 0;
+int offset = 0;
 
-int noHandTimes;
-int number;
+float duration, dist;
 
 double recordedDists[200];
+int recordedDistLength = 0;
 float deltaDist;
 
-int recordedDistLength = 0;
 int hasHandCount = 0;
 int noHandCount = 0;
-float h1 = 0;
-float h2 = 0;
+double h1 = 0;
+double h2 = 0;
 
 int hue = 0;
 int saturation = 0;
 int brightness = 0;
 
-bool hasChanged = false;
-
-
-int offset = 0;
-
-int dot = 0;
-
-int color_state = 0;
-int pattern_state = 0;
-
-
 void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS);
-  
-  pinMode(TRIG_PIN, OUTPUT); // Sets the trigPin as an Output
-  pinMode(ECHO_PIN, INPUT); // Sets the echoPin as an Input
-  Serial.begin(9600); // Starts the serial communication
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  Serial.begin(9600);
+}
 
-  noHandTimes = 0;
-  number = 0;
+float toCm(float duration) {
+  return (duration/2)/29.1; // Divide by 29.1 or multiply by 0.0343
+}
+
+float toInches(float duration) {
+  return (duration/2)/74; // Divide by 74 or multiply by 0.0135
 }
 
 void loop() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  // the sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(5);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
  
-  // Read the signal from the sensor: a HIGH pulse whose
+  // read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
   pinMode(ECHO_PIN, INPUT);
   duration = pulseIn(ECHO_PIN, HIGH);
+  dist = toCm(duration);     
   
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  //  Serial.println(cm);
-  //inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-  
-  if(hasHandCount < 15) {
-    if(cm < 15) {
+  if(hasHandCount < 15) { // start detecting hands
+    if(dist < 15) {
       hasHandCount += 1;
     } else {
       hasHandCount = 0;
     }
-  } else if(hasHandCount == 15) {
-    if(cm < 15 && recordedDistLength <= 200) {
-      recordedDists[recordedDistLength] = cm;
+  } else if(hasHandCount == 15) { // has detected hands for a while
+    if(dist < 15 && recordedDistLength <= 200) {
+      recordedDists[recordedDistLength] = dist;
       if(recordedDistLength > 0) {
         deltaDist = abs(recordedDists[recordedDistLength] - recordedDists[recordedDistLength-1]);
         if(deltaDist > 0.25) {
-          recordedDists[recordedDistLength] = cm;
+          recordedDists[recordedDistLength] = dist;
           recordedDistLength += 1;
         }
-      } else {
-        recordedDists[recordedDistLength] = cm;
+      } else { // save hands positions in the array
+        recordedDists[recordedDistLength] = dist;
         recordedDistLength += 1;
       }
-    } else if(recordedDistLength > 0) {
+    } else if(recordedDistLength > 0) { // dectect hands leaving
       noHandCount += 1;
       if (noHandCount>= 15) {
         for(int i = 0; i < recordedDistLength; i++) {
@@ -99,12 +90,15 @@ void loop() {
         h1 = h1/int(recordedDistLength/2);
         h2 = h2/(recordedDistLength - int(recordedDistLength/2));
         if(h1 > h2) {
-          Serial.println("left!");
+          // Serial.println("left!");
+          if(pattern_state == 0) {
+            color_state = 0;
+          }
           pattern_state = (pattern_state+1)%4;
-          hasChanged = false;
+          hasBlackedOut = false;
         } else {
-          Serial.println("right!");
-          color_state = (color_state+1)%8;
+          // Serial.println("right!");
+          color_state = (color_state+1)%7;
         }
         hasHandCount = 0;
         noHandCount = 0;
@@ -114,47 +108,39 @@ void loop() {
       }
     } 
   }
-  delay(5);
-
-  int b = 0;
 
   switch(color_state) {
-    case 0: // white
-      hue = 0;
-      saturation = 0;
-      brightness = 200;
-      break;
-    case 1: // red
+    case 0: // red
       hue = 95;
       saturation = 255;
       brightness = 200;
       break;
-    case 2: // yellow
+    case 1: // yellow
       hue = 75;
       saturation = 255;
       brightness = 200;
       break;
-    case 3: // green
+    case 2: // green
       hue = 0;
       saturation = 255;
       brightness = 200;
       break;
-    case 4: // cyan
+    case 3: // cyan
       hue = 230;
       saturation = 255;
       brightness = 200;
       break;
-    case 5: // blue
+    case 4: // blue
       hue = 175;
       saturation = 255;
       brightness = 200;
       break;
-    case 6: // purple
+    case 5: // purple
       hue = 150;
       saturation = 255;
       brightness = 200;
       break;
-    case 7: // rainbow
+    case 6: // rainbow
       hue = (hue+4)%255;;
       saturation = 255;
       brightness = 200;
@@ -163,9 +149,8 @@ void loop() {
 
   switch(pattern_state) {
     case 0: // off
-      if(hasChanged == false) {
-        hasChanged = true;
-        color_state = 0;
+      if(hasBlackedOut == false) {
+        hasBlackedOut = true;
         for(int i=0; i<NUM_LEDS; i++) {
           leds[i] = CRGB::Black;
         }
@@ -183,7 +168,7 @@ void loop() {
       }
       FastLED.show();
       break;
-    case 2: // counter clockwise
+    case 2: // counter-clockwise
       for(int i=0; i<NUM_LEDS; i++) {
         if(255-(i*5) > 0) {
           brightness = 255-(i*5);
@@ -204,8 +189,7 @@ void loop() {
       }
       break;   
   }
-
+  
   offset = (offset+1)%NUM_LEDS;
-   
-  delay(5);
+  delay(10);
 }
